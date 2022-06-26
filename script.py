f8e6f13e294188e61a8c2a1d7339ca7d13d1ad6e
TylerEnglish <77080632+TylerEnglish@users.noreply.github.com>
2022-06-25 19:46:39 -0600

    frame of the script file

---
+++ script.py
@@ -0,0 +1,64 @@
+"""
+Read files
+filter/search/join files
+open model
+
+"""
+
+def read():
+    """
+    Read student and tutor data
+    """
+    pass
+
+def save():
+    """
+    Save time slot to 
+    student data
+    """
+
+    pass
+
+def find():
+    """
+    Find classes related to 
+    the student failing classes
+    """
+    def filter():
+        """
+        filter out all tutors that 
+        don't match the fail classes
+        """
+        pass
+
+    def search():
+        """
+        Select all tutors
+        with related classes
+        """
+        pass
+
+    def join():
+        """
+        Join all the tutors
+        together
+        """
+        pass
+
+    pass
+
+def launch_bot():
+    """
+    load model and startup the 
+    chat bot
+    """
+    pass
+
+def populate_appt_table():
+    """
+    merge tutor and student info into one comprehensive table for the tutoring center
+    """
+    pass
+
+def main():
+    pass
